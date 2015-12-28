664c085de0ba0fbea69e93704368cf151bacb72e
LisaLichtenberger <lisa.lichtenberger9@gmail.com>
2015-12-28 09:11:45 +0100

    Tutorial wird nur bei erstem Launch der App angezeigt --> Shared Preferences SplashScreen ActionBar Item hinzugefügt Grundstruktur für ViewPager gebaut

--- app/src/main/java/at/fhooe/mc/android/itsme/Profile.java
+++ app/src/main/java/at/fhooe/mc/android/itsme/Profile.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Tags.OnFragmentInteractionListener} interface
+ * {@link Profile.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Tags#newInstance} factory method to
+ * Use the {@link Profile#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Tags extends Fragment {
+public class Profile extends Fragment {
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 	private static final String ARG_PARAM1 = "param1";
@@ -35,11 +35,11 @@ public class Tags extends Fragment {
 	 *
 	 * @param param1 Parameter 1.
 	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment Tags.
+	 * @return A new instance of fragment Profile.
 	 */
 	// TODO: Rename and change types and number of parameters
-	public static Tags newInstance(String param1, String param2) {
-		Tags fragment = new Tags();
+	public static Profile newInstance(String param1, String param2) {
+		Profile fragment = new Profile();
 		Bundle args = new Bundle();
 		args.putString(ARG_PARAM1, param1);
 		args.putString(ARG_PARAM2, param2);
@@ -47,7 +47,7 @@ public class Tags extends Fragment {
 		return fragment;
 	}
 
-	public Tags() {
+	public Profile() {
 		// Required empty public constructor
 	}
 
@@ -63,7 +63,7 @@ public class Tags extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_tags, container, false);
+		return inflater.inflate(R.layout.fragment_profile, container, false);
 	}
 
 	// TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/at/fhooe/mc/android/itsme/SplashScreen.java
+++ app/src/main/java/at/fhooe/mc/android/itsme/SplashScreen.java
@@ -2,57 +2,50 @@ package at.fhooe.mc.android.itsme;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class SplashScreen extends Activity {
 
 	private static final String TAG = "Itsme";
 	Handler mHandler = new Handler();
+	final String PREFS_NAME = "firstStart";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		ActionBar ab = getActionBar();
 		ab.hide();
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
+
+		final SharedPreferences settings = this.getPreferences(Context.MODE_PRIVATE);
 
 		mHandler.postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				Log.i(TAG, "now in Thread");
-				Intent i = new Intent(SplashScreen.this, HomeScreenActivity.class);
-				Log.i(TAG, "startActivity");
-				startActivity(i);
-				finish();
+				if (settings.getBoolean("firstLaunch", true)) {
+					//the app is being launched for first time, do something
+					Log.d("Comments", "First time");
+
+					Intent i = new Intent(SplashScreen.this, Tutorial.class);
+					startActivity(i);
+					finish();
+
+					settings.edit().putBoolean("firstLaunch", false).commit();
+				} else {
+					Intent i = new Intent(SplashScreen.this, HomeScreenActivity.class);
+					startActivity(i);
+					finish();
+				}
+
 			}
 		}, 3000);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 }
